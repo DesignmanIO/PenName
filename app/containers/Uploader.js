6c33a5fef2ed7f42c8dc30181339f10a5bdad569
Julian Kingman <julian@designman.io>
2017-02-11 18:37:21 -0500

    finished upload view

--- app/containers/Uploader.js
+++ app/containers/Uploader.js
@@ -5,17 +5,19 @@ import Home from '../components/Home';
 const {Content} = Layout;
 const {Dragger} = Upload;
 
-export default class HomePage extends Component {
+export default class Uploader extends Component {
   render() {
     return (
       <Content style={{padding: 15}}>
+        <div style={{height: 180}}>
           <Dragger className="uploader">
             <p className="ant-upload-drag-icon">
               <Icon type="file-text"/>
             </p>
-            <p className="ant-upload-text">Click or drag file to this area to upload</p>
-            <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
+            <p className="ant-upload-text">Click or drag file to this area to check</p>
+          <p className="ant-upload-hint">Supporder file types: .txt</p>
           </Dragger>
+        </div>
       </Content>
     );
   }
